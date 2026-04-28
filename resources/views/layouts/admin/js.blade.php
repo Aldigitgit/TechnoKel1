7defb28e90085d7217068f4a26b2c271612378a1
Aldi <Aldisama789@gmail.com>
2026-04-28 15:09:48 +0700

    Revisi08

--- resources/views/layouts/admin/js.blade.php
+++ resources/views/layouts/admin/js.blade.php
@@ -1,9 +1,9 @@
    <!-- Core -->
-   <script src="../../vendor/@popperjs/core/dist/umd/popper.min.js"></script>
-   <script src="../../vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+   <script src="{{ asset('Assets-Admin/vendor/@popperjs/core/dist/umd/popper.min.js') }}"></script>
+   <script src="{{ asset('Assets-Admin/vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 
    <!-- Vendor JS -->
-   <script src="../../vendor/onscreen/dist/on-screen.umd.min.js"></script>
+   <script src="{{ asset('Assets-Admin/vendor/onscreen/dist/on-screen.umd.min.js') }}"></script>
 
    {{-- chart js --}}
    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
